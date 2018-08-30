4f505a1206aced605d2d721dc509946eeeb2b8c1
ivorgri <39122253+ivorgri@users.noreply.github.com>
2018-08-30 07:33:53 +0200

    Create initial page preview component

---
+++ components/PagePreview.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="VueToNuxtLogo">
+    <h2>h2</h2>
+    <p>p</p>
+    <div class="Triangle Triangle--three"/>
+    <div class="Triangle Triangle--four"/>
+  </section>
+</template>
