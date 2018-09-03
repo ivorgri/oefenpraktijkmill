798a30f7f58909112e4124afa78058bd55ee2ae6
ivorgri <ivorgrisel@gmail.com>
2018-09-03 20:36:19 +0200

    Added asyncData to slug, as well as setup for payload to be used with generate

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,8 +1,19 @@
 <template>
   <main>
-    <p>
-      Test
-    </p>
+    <div v-html="page.content.rendered"/>
   </main>
 </template>
 
+<script>
+export default {
+  async asyncData ({ app, params, error, payload }) {
+    let page;
+    if (payload) { page = payload; }
+    else {
+      page = await app.$axios.$get(`http://oefenpraktijkmill.ivrdesign.nl/wp-json/wp/v2/pages/?slug=${params.slug}`) ;
+      page = page[0];
+    }
+    return { page };
+  }
+}
+</script>
