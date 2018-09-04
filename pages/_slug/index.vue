3e77b1a2f7986cb4150b6e60ed197e7957b47a11
ivorgri <ivorgrisel@gmail.com>
2018-09-04 09:37:05 +0200

    Moved contact form to component and updated slug page and store to check if page is a contact page (and add the form to the page)

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,14 +1,23 @@
 <template>
   <main>
     <div v-if="page" v-html="page.content.rendered"/>
+    <contact-form v-if="isContactPage"></contact-form>
   </main>
 </template>
 
 <script>
+import ContactForm from '~/components/ContactForm.vue'
+
 export default {
+  components: {
+    ContactForm,
+  },
   computed: {
     page() {
       return this.$store.getters.getPage(this.$route.params.slug);
+    },
+    isContactPage() {
+      return this.$store.getters.isPageContactForm(this.page.id);
     },
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,16 +1,21 @@
 import Vuex from 'vuex';
 import axios from 'axios';
 
-const pagesApi = 'http://oefenpraktijkmill.ivrdesign.nl/wp-json/wp/v2/pages';
+const pagesApi = 'https://oefenpraktijkmill.ivrdesign.nl/wp-json/wp/v2/pages';
+const afcPagesApi = 'https://oefenpraktijkmill.ivrdesign.nl//wp-json/acf/v3/pages ';
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      pages: {},
+      pages: [],
+      customPageFields: [],
     },
     mutations: {
       SET_PAGES (state, pages) {
         state.pages = pages;
+      },
+      SET_CUSTOM_PAGE_FIELDS (state, customPageFields) {
+        state.customPageFields = customPageFields;
       },
     },
     getters: {
@@ -20,13 +25,20 @@ const createStore = () => {
         });
       },
       getPage: (state) => (slug) => {
-        return state.pages.find((page) => page.slug == slug);
-      }
+        return state.pages.find((page) => page.slug === slug);
+      },
+      isPageContactForm: (state) => (pageId) => {
+        const page = state.customPageFields.find((page) => page.id === pageId);
+        return page.acf.is_contactformulier;
+      },
     },
     actions: {
       async nuxtServerInit ({ commit }) {
-        let { data } = await axios.get(pagesApi);
-        commit('SET_PAGES', data);
+        let pages, customPageFields;
+        pages = await axios.get(pagesApi);
+        commit('SET_PAGES', pages.data);
+        customPageFields = await axios.get(afcPagesApi);
+        commit('SET_CUSTOM_PAGE_FIELDS', customPageFields.data);
       }
     }
   })
