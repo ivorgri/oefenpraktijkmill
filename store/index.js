81b2bfa0377a72f2a769ff1465500c66c0e52c14
ivorgri <ivorgrisel@gmail.com>
2018-09-05 21:31:55 +0200

    Updated store to retrieve company address

--- store/index.js
+++ store/index.js
@@ -2,15 +2,20 @@ import Vuex from 'vuex';
 import axios from 'axios';
 
 const pagesApi = 'https://oefenpraktijkmill.ivrdesign.nl/wp-json/wp/v2/pages';
+const addressesApi = 'https://oefenpraktijkmill.ivrdesign.nl/wp-json/wp/v2/addresses';
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
       pages: [],
+      address: {},
     },
     mutations: {
       SET_PAGES (state, pages) {
         state.pages = pages;
+      },
+      SET_ADDRESS (state, address) {
+        state.address = address;
       },
     },
     getters: {
@@ -22,12 +27,18 @@ const createStore = () => {
       getPage: (state) => (slug) => {
         return state.pages.find((page) => page.slug === slug);
       },
+      getAddress (state) {
+        return state.address;
+      },
     },
     actions: {
       async nuxtServerInit ({ commit }) {
         let pages;
         pages = await axios.get(pagesApi);
         commit('SET_PAGES', pages.data);
+        let address;
+        address = await axios.get(addressesApi);
+        commit('SET_ADDRESS', address.data[0].acf);
       }
     }
   })
