c361f0922386db728f27c834165e6086e22749b3
ivorgri <ivorgrisel@gmail.com>
2018-09-18 10:05:05 +0200

    Replaced main design by test design. Removed test files.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,10 @@
 <template>
   <div id="main-content">
     <header>
-      <nuxt-link to="/">
-        <h1 class="title">
-          Oefenpraktijk Mill
-        </h1>
-      </nuxt-link>
-      <div id="dropdown-menu" :class="{ 'change': toggleAnimation }" @click="toggleNavMenu">
-        <div id="bar1"></div>
-        <div id="bar2"></div>
-        <div id="bar3"></div>
-      </div>
-      <nav v-if="showNavMenu">
-        <nuxt-link v-for="page in $store.getters.pages"
-          :key="page.id"
-          :to="'/'+page.slug"
-          class="nav-item">
-          {{ page.title.rendered }}
-        </nuxt-link>
-      </nav>
+      <img id="header-image" :src="homepageImageUrl" :alt="homepageImageAlt"/>
+      <h1 id="header-title">Oefenpraktijk Dianne van Dongen</h1>
+      <button id="header-button-1" class="header-button" @click="scrollTo('contact')">Inschrijven</button>
+      <button id="header-button-2" class="header-button" @click="scrollTo('main')">Informatie</button>
     </header>
     <nuxt/>
     <footer>
@@ -36,37 +22,25 @@
 
 <script>
 export default {
-  mounted() {
-    this.handleResize();
-    window.addEventListener('resize', this.handleResize);
-  },
-  data() {
-    return {
-      showNavMenu: undefined,
-      toggleAnimation: false,
-    };
-  },
   computed: {
     address() {
       return this.$store.getters.getAddress;
     },
+    homepageImage() {
+      return this.$store.getters.getHomepage["_embedded"]["wp:featuredmedia"][0].media_details.sizes["twentyseventeen-featured-image"];
+    },
+    homepageImageUrl() {
+      return (this.homepageImage.source_url ? this.homepageImage.source_url : '');
+    },
+    homepageImageAlt() {
+      return (this.homepageImage.alt_text ? this.homepageImage.alt_text: '');
+    },
   },
   methods: {
-    toggleNavMenu () {
-      this.showNavMenu = !this.showNavMenu;
-      this.toggleAnimation = this.showNavMenu;
+    scrollTo(elementId) {
+      const element = document.getElementById(elementId);
+      element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'start'});
     },
-    handleResize() {
-      if(document.documentElement.clientWidth > 649) {
-        this.toggleAnimation = false;
-        this.showNavMenu = true;
-      } else {
-        this.showNavMenu = false;
-      }
-    },
-  },
-  beforeDestroy() {
-    window.removeEventListener('resize', this.handleResize);
   },
 }
 </script>
@@ -74,64 +48,103 @@ export default {
 <style lang="scss">
   @import '~assets/variables.scss';
 
-  .title {
-    width: 275px;
-    min-width: 275px;
-    float: left;
+  header {
+    display: grid;
+    grid-template-columns: [start-col] 1fr 1fr 1fr 1fr [end-col];
+    grid-column-gap: 100px;
+    grid-template-rows: [start-row] 25vh 30vh 5vh 20vh 20vh [end-row];
+    grid-template-areas: "none none none none"
+      "title title title title"
+      "empty3 empty3 empty3 empty3"
+      "empty4 button1 button2 empty5"
+      "empty6 empty6 empty6 empty6";
+    height: 100vh;
+    width: 100%;
+    overflow: hidden;
+    justify-items: center;
+    align-items: center;
   }
 
-  #dropdown-menu {
-    @media screen and (min-width: $break-medium) {
-      display: none;
+  #header-image {
+    grid-area: start-row / start-col / end-row / end-col;
+    width: 100vw;
+    filter: opacity(80%) grayscale(25%);
+  }
+
+  @supports (filter: opacity(80%)) and (filter: grayscale(25%)) {
+    #header-image {
+      filter: opacity(80%) grayscale(25%);
     }
-    cursor: pointer;
-    float: right;
-    margin: 18px 15px;
   }
 
-  #bar1, #bar2, #bar3 {
-    width: 35px;
-    height: 5px;
-    background-color: #333;
-    margin: 6px 0;
-    transition: 0.4s;
-  }
-
-  .change #bar1 {
-    -webkit-transform: rotate(-45deg) translate(-9px, 6px);
-    transform: rotate(-45deg) translate(-9px, 6px);
-  }
-
-  .change #bar2 {opacity: 0;}
-
-  .change #bar3 {
-    -webkit-transform: rotate(45deg) translate(-8px, -8px);
-    transform: rotate(45deg) translate(-8px, -8px);
-  }
-
-  nav {
-    @supports (display: flex) {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: space-evenly;
+  @supports (clip-path: polygon($header-image-clip-path)) or (-webkit-clip-path: polygon($header-image-clip-path)) {
+    header {
+      -webkit-clip-path: polygon($header-image-clip-path);
+      clip-path: polygon($header-image-clip-path);
     }
-    /* @media screen and (max-width: $break-small) {
-      width: 100%;
-    }
-    @media screen and (max-width: $break-medium) {
-      display: none;
-    } */
-    clear: both;
+  }
 
-    .nav-item {
-      padding: 5px;
-      color: black;
+  #header-title {
+    font-size: 50px;
+  }
 
-      &:hover {
-        color: red;
-      }
+  .header-button {
+    width: 350px;
+    height: 50px;
+    z-index: 10;
+    font-size: 35px;
+    color: white;
+    border: 2px solid white;
+    border-radius: 10px;
+    background-color: rgba(192,192,192,0.5);
+    cursor:pointer;
+    text-shadow: 1px 1px rgb(192,192,192);
+
+    &:hover {
+      background-color: rgba(192,192,192,0.9);
     }
+  }
+
+  @media screen and (min-width: 1160px) {
+    #header-title {
+      font-size:70px;
+    }
+
+    .header-button {
+      width: 350px;
+      height: 50px;
+      font-size: 35px;
+    }
+  }
+  @media screen and (max-width: 1160px) {
+    #header-title {
+      font-size:50px;
+    }
+
+    .header-button {
+      width: 250px;
+      height: 40px;
+      font-size: 25px;
+    }
+  }
+
+  #header-title {
+    grid-area: title;
+
+    margin: 0;
+    z-index: 10;
+    color: white;
+    text-shadow: 1px 1px rgb(192,192,192);
+    height:100%;
+
+  }
+
+  #header-button-1 {
+    grid-area: button1;
+  }
+
+  #header-button-2 {
+    grid-area: button2;
   }
 
   footer {
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <main>
-    <page-preview v-for="page in $store.getters.subPages"
+  <main id="main"
+    :style="templateRows">
+    <page-preview v-for="(page, index) in $store.getters.subPages"
       :key="page.id"
       :title="page.title.rendered"
       :excerpt="page.excerpt.rendered"
       :homepageText="page.acf.homepage_tekst"
-      :image="page._embedded['wp:featuredmedia']"
-      :slug="page.slug"/>
+      :image="pageImage(page)"
+      :slug="page.slug"
+      :style="setStartingRow(index)"/>
   </main>
 </template>
 
@@ -14,9 +16,46 @@
 import PagePreview from '~/components/PagePreview.vue'
 
 export default {
+  layout: 'test',
   components: {
     PagePreview,
+  },
+  computed: {
+    templateRows() {
+      const imageOverlap = '30px'
+      let templateRows = `${imageOverlap} auto ${imageOverlap}`;
+      for (let i = 1; i < this.$store.getters.subPages.length; i++) {
+        templateRows += ` auto ${imageOverlap}`;
+      }
+      return {
+        'grid-template-rows': `${templateRows}`,
+      }
+    }
+  },
+  methods: {
+    setStartingRow(index) {
+      const startingRow = index + (index + 1);
+      return {
+        'grid-row': `${startingRow} / span 3`,
+      }
+    },
+    pageImage(page) {
+      const images = page['_embedded']['wp:featuredmedia'];
+      if(images) {
+        return images[0].media_details.sizes.medium_large;
+      }
+      return {};
+    }
   },
 }
 </script>
 
+<style lang="scss">
+  @import '~assets/variables.scss';
+
+  #main {
+    padding: 25px 0;
+    display: grid;
+    grid-template-columns: auto;
+  }
+</style>
